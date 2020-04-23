58e9a0a3c361e27f46b24bcf5cce54b7a39e90db
10xOXR <10xoxr@gmail.com>
2020-04-23 21:42:18 +0100

    Bump pillow to v6.2.2

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -7,6 +7,8 @@ from django.contrib.auth.views import (
 urlpatterns = [
     path("", PasswordResetView.as_view(), name="password_reset"),
     path("done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("<uidb64>/<token>", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("complete/", PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path("<uidb64>/<token>", PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+    path("complete/", PasswordResetCompleteView.as_view(),
+         name="password_reset_complete")
 ]
